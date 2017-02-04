77fd9f30c4c957ee2033d643a31343f9e26f8728
the-nerbs <scott.nerbetski@gmail.com>
2017-02-04 14:22:56 -0500

    Changed MultiplyConverter to support conversions to/from GridLength, as this is needed for WPF's DataGrid.

--- VSCoverageViewer/Converters/MultiplyConverter.cs
+++ VSCoverageViewer/Converters/MultiplyConverter.cs
@@ -9,9 +9,12 @@ using System.Windows.Data;
 namespace VSCoverageViewer.Converters
 {
     /// <summary>
-    /// A value converter used to multiply a numeric value by a given factor.
+    /// A value converter used to multiply a numeric value by a given factor. Supports converting
+    /// from any <see cref="IConvertible"/> supporting <see cref="IConvertible.ToDouble(IFormatProvider)"/>
+    /// to either double or GridLength.
     /// </summary>
     [ValueConversion(typeof(IConvertible), typeof(double))]
+    [ValueConversion(typeof(IConvertible), typeof(GridLength))]
     internal class MultiplyConverter : IValueConverter
     {
         /// <summary>
@@ -26,15 +29,23 @@ namespace VSCoverageViewer.Converters
         /// <param name="value">The value to convert.</param>
         /// <param name="targetType">The type to convert to. Must be double.</param>
         /// <param name="parameter">Unused.</param>
-        /// <param name="culture">Unused.</param>
+        /// <param name="culture">The culture to perform the conversion in.</param>
         /// <returns>The given value, multiplied by the factor.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is IConvertible &&
-                targetType == typeof(double))
+            if (value is IConvertible)
             {
-                double val = ((IConvertible)value).ToDouble(null);
-                return val * Factor;
+                double val = ((IConvertible)value).ToDouble(culture);
+                double result = val * Factor;
+
+                if (targetType == typeof(double))
+                {
+                    return result;
+                }
+                else if (targetType == typeof(GridLength))
+                {
+                    return new GridLength(result);
+                }
             }
 
             return DependencyProperty.UnsetValue;
@@ -51,11 +62,22 @@ namespace VSCoverageViewer.Converters
         /// <returns>The given value, multiplied by the factor.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is IConvertible &&
-                typeof(IConvertible).IsAssignableFrom(targetType))
+            if (typeof(IConvertible).IsAssignableFrom(targetType))
             {
-                double val = ((IConvertible)value).ToDouble(null);
-                return val / Factor;
+                if (value is double)
+                {
+                    double val = ((IConvertible)value).ToDouble(null);
+                    return val / Factor;
+                }
+                else if (value is GridLength)
+                {
+                    var gridLength = (GridLength)value;
+
+                    if (gridLength.GridUnitType == GridUnitType.Pixel)
+                    {
+                        return gridLength.Value / Factor;
+                    }
+                }
             }
 
             return DependencyProperty.UnsetValue;
